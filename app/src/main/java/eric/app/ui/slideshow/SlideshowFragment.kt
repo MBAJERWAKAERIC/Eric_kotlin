cb2fefbc539b567b26f7eb3dfecc41239ed246d5
mbajerwakaeric <erickmbanjerwaka@gmail.com>
2022-12-27 17:52:21 +0300

    Initial commit

---
+++ app/src/main/java/eric/app/ui/slideshow/SlideshowFragment.kt
@@ -0,0 +1,42 @@
+package eric.app.ui.slideshow
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import android.support.v4.app.Fragment
+import android.arch.lifecycle.ViewModelProvider
+import eric.app.databinding.FragmentSlideshowBinding
+
+class SlideshowFragment : Fragment() {
+
+    private var _binding: FragmentSlideshowBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View {
+        val slideshowViewModel =
+                ViewModelProvider(this, ViewModelProvider.NewInstanceFactory()).get(SlideshowViewModel::class.java)
+
+        _binding = FragmentSlideshowBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textSlideshow
+        slideshowViewModel.text.observe(viewLifecycleOwner) {
+            textView.text = it
+        }
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
